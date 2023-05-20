2d5ed60d83a43f1813267e071694f3a7e9a93999
jrobertorma <jrobertorma@gmail.com>
2023-05-20 07:09:03 -0600

    Add routing to the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,50 @@ import { ReduxCounter } from './ReduxCounter';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import {PostsList} from './PostsList';
+import {createBrowserRouter, RouterProvider, Outlet, Link} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/counter`}>Counter</Link>
+            </li>
+            <li>
+              <Link to={`/posts`}>Posts</Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </div>,
+    errorElement:
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </div>,
+    children: [
+      {
+        path: "counter",
+        element: <ReduxCounter />
+      },
+      {
+        path: "posts",
+        element: <PostsList />
+      }
+    ]
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
+      <RouterProvider router={router} />
       <div className="App">
-        <div>
-          <ReduxCounter />
-        </div>
-        <div>
-          <PostsList />
-        </div>
       </div>
     </Provider>
   );
